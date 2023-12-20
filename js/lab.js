3ee65a51824763a167f3f1dea6e4532cd78a5924
mhyeseon <20220826@dongduk.ac.kr>
2023-12-21 06:06:13 +0900

    image

---
+++ js/lab.js
@@ -0,0 +1,10 @@
+var grade="B", gpa;
+
+switch(grade) {
+   case "A" : gpa=4.0;
+   case "B" : gpa=3.0;
+   case "C" : gpa=2.0;
+    case "D" : gpa=1.0;
+    default: gpa=0.0;
+}
+console.log("gpa: "+gpa) ;
